fe24f4b7bf6c4742d8be3cf5e305215c9cb67f89
StealthTom <seafordtbf@gmail.com>
2018-02-03 02:41:25 -0500

    Setup a new UI element for system notifications like losing connection or not having java.

---
+++ Assets/Scripts/UI/NotificationPopup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using ParallelProg.UI;
+using UnityEngine.UI;
+
+public class NotificationPopup : MonoBehaviour {
+
+    [SerializeField]
+    GameObject buttonPrefab;
+    [SerializeField]
+    Transform buttonContainer;
+    [SerializeField]
+    Button exitButton;
+    [SerializeField]
+    Button[] buttons;
+    [SerializeField]
+    Text title;
+    [SerializeField]
+    Text body;
+
+    public void SetNotification(string _title, string _body, bool enableExit, UnityEngine.Events.UnityAction[] actions)
+    {
+        title.text = _title;
+        body.text = _body;
+        exitButton.gameObject.SetActive(enableExit);
+
+        buttons = new Button[actions.Length];
+        for(int i = 0; i < actions.Length; i++)
+        {
+            int index = i;
+            GameObject temp = GameObject.Instantiate(buttonPrefab);
+            buttons[index] = temp.GetComponent<Button>();
+            buttons[index].onClick.AddListener(actions[index]);
+        }
+    }
+
+}
