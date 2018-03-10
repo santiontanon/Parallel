847ed0990f561a465dc3f69831f5ad35910fdfbf
StealthTom <seafordtbf@gmail.com>
2018-03-10 01:40:45 -0500

    Added function for copying a TimeStepData object.

--- Assets/Scripts/Level/LevelData/TimeStepData.cs
+++ Assets/Scripts/Level/LevelData/TimeStepData.cs
@@ -11,11 +11,50 @@ public class TimeStepData{
     public List<DeliveryData> deliveryPoints;
     public List<SemaphoreData> sempahores;
 
+    public TimeStepData()
+    {
+        threads = new List<ThreadData>();
+        packages = new List<PackageData>();
+        deliveryPoints = new List<DeliveryData>();
+        sempahores = new List<SemaphoreData>();
+    }
+
+    public TimeStepData Copy(TimeStepData _timeStep)
+    {
+        TimeStepData timeStep = new TimeStepData();
+        for (int i = 0; i < _timeStep.threads.Count; i++)
+        {
+            ThreadData thread = new ThreadData();
+            thread.id = _timeStep.threads[i].id;
+            thread.pos = new Vector2(_timeStep.threads[i].pos.x, _timeStep.threads[i].pos.y);
+            thread.rotation = new Vector2(_timeStep.threads[i].rotation.x, _timeStep.threads[i].rotation.y);
+            timeStep.threads.Add(thread);
+        }
+        for (int i = 0; i < _timeStep.deliveryPoints.Count; i++)
+        {
+            DeliveryData delivery = new DeliveryData();
+            delivery.id = _timeStep.deliveryPoints[i].id;
+            delivery.deliveries = _timeStep.deliveryPoints[i].deliveries;
+            timeStep.deliveryPoints.Add(delivery);
+        }
+        for (int i = 0; i < _timeStep.sempahores.Count; i++)
+        {
+            SemaphoreData semaphore = new SemaphoreData();
+            semaphore.id = _timeStep.sempahores[i].id;
+            semaphore.open = _timeStep.sempahores[i].open;
+            timeStep.sempahores.Add(semaphore);
+        }
+        return timeStep;
+    }
+
     public ThreadData GetThread(int id)
     {
         for(int i = 0; i < threads.Count; i++)
         {
-
+            if(threads[i].id == id)
+            {
+                return threads[i];
+            }
         }
         return null;
     }
@@ -24,7 +63,10 @@ public class TimeStepData{
     {
         for(int i = 0; i < deliveryPoints.Count; i++)
         {
-
+            if (deliveryPoints[i].id == id)
+            {
+                return deliveryPoints[i];
+            }
         }
         return null;
     }
@@ -33,7 +75,10 @@ public class TimeStepData{
     {
         for(int i = 0; i < sempahores.Count; i++)
         {
-
+            if (sempahores[i].id == id)
+            {
+                return sempahores[i];
+            }
         }
         return null;
     }
@@ -68,5 +113,5 @@ public class DeliveryData
 public class SemaphoreData
 {
     public int id;
-    public bool open;
+    public int open;
 }
