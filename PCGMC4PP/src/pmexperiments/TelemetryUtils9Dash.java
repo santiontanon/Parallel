1e17f11e4a4c2c490262befd212c1d320fd2669e
Pavan Kantharaju <pavank92@gmail.com>
2018-10-22 13:13:17 -0400

    Forgot to add this file

---
+++ PCGMC4PP/src/pmexperiments/TelemetryUtils9Dash.java
@@ -0,0 +1,17 @@
+package pmexperiments;
+
+import playermodeling.TelemetryUtils;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class TelemetryUtils9Dash extends TelemetryUtils {
+
+    public TelemetryUtils9Dash() {}
+
+    @Override
+    public LinkedHashMap<String, ArrayList<String>> getLevelData(String data_path) {
+        LinkedHashMap<String, ArrayList<String> > tmp = new LinkedHashMap<>();
+        return tmp;
+    }
+}
