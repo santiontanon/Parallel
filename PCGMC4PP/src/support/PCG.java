005620cd376709cee7360eedd70a874240a65676
Santiago Ontañón <santi.ontanon@gmail.com>
2019-01-04 09:15:31 -0800

    added a new optional parameter to PCG for output path

--- PCGMC4PP/src/support/PCG.java
+++ PCGMC4PP/src/support/PCG.java
@@ -82,6 +82,9 @@ public class PCG {
         if (args.length >= 4) {
             outputPath = args[3];
             dataFolderPath = args[3];
+            if (args.length >= 5) {
+                outputPath = args[4];
+            }
         }
         for(int i = 0;i<args.length;i++) {
             if (args[i].equals("-pcgid")) {
