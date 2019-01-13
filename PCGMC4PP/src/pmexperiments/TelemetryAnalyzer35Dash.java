09b6711ac998ab0a182cf9d9bf2c7f67a3677186
Pavan Kantharaju <pavank92@gmail.com>
2019-01-12 20:36:29 -0500

    Clean up experiment code

--- PCGMC4PP/src/pmexperiments/TelemetryAnalyzer35Dash.java
+++ PCGMC4PP/src/pmexperiments/TelemetryAnalyzer35Dash.java
@@ -8,39 +8,6 @@ public class TelemetryUtils35Dash extends TelemetryUtils {
 
     public TelemetryUtils35Dash() {}
 
-//    public String getUsername(ArrayList<String> data) {
-//        /*  NOTE: There are a few users from the study
-//            (3 and 5 dash datasets) that were not correctly mentioned in the "SessionUser" event..
-//            */
-//
-//        String session_id = "";
-//        for ( String d : data ) {
-//            String [] tmp = d.split("\t");
-//            if ( tmp[1].equals("SessionID") ) {
-//                session_id = tmp[2];
-//            }
-//
-//            if ( tmp[1].equals("SessionUser") ) {
-//                if ( tmp[2].equals("NONE")  ) {
-//                    /* Check session id */
-//                    switch ( session_id ) {
-//                        case "CAT":
-//                            return "MOUSE";
-//                        case "DOG":
-//                            return "DRAGON";
-//                    }
-//                }
-//                if ( tmp[2].equals("CHICKEN") ) {
-//                    return "MONKEY";
-//                }
-//                return tmp[2];
-//            }
-//        }
-//        System.err.println("Execution should never be here.");
-//        System.exit(-1);
-//        return "";
-//    }
-
     public String getUsername(ArrayList<String> data) {
         /*  NOTE: There are a few users from the study
             (3 and 5 dash datasets) that were not correctly mentioned in the "SessionUser" event..
@@ -57,14 +24,14 @@ public class TelemetryUtils35Dash extends TelemetryUtils {
                 if ( tmp[2].equals("NONE")  ) {
                     /* Check session id */
                     switch ( session_id ) {
-                        case "1241":
-                            return "3-1";
-                        case "1238":
-                            return "3-4";
+                        case "CAT":
+                            return "MOUSE";
+                        case "DOG":
+                            return "DRAGON";
                     }
                 }
-                if ( tmp[2].equals("evan") ) {
-                    return "5-1";
+                if ( tmp[2].equals("CHICKEN") ) {
+                    return "MONKEY";
                 }
                 return tmp[2];
             }
