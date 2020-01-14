4e354545fc97b3a00427c5285b5e409ca47c1dcf
StealthTom <seafordtbf@gmail.com>
2020-01-14 10:38:29 -0500

    Work on implementing the circle graphs.

---
+++ Assets/Scripts/UI/UICircleGraph.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerModel : MonoBehaviour {
+
+    Transform CircleParent;
+    Image CirclePrefab;
+    List<Image> Circles;
+
+    void Init(string title, List<float> values)
+    {
+        CreateGraph(values);
+    }
+
+    void CreateGraph(List<float> values)
+    {
+        float total = 0f;
+        foreach(float f in values)
+            total += f;
+
+        float current = 0f;
+        foreach(float f in values)
+        {
+            Image circle = Instantiate(CirclePrefab, CircleParent);
+            circle.fillAmount = (f / total);
+            circle.rectTransform.eulerAngles = new Vector3(0f, 0f, current);
+            current += f / total;
+            Circles.Add(circle);
+        }
+    }
+
+    void ClearGraph()
+    {
+        foreach(Image i in Circles)
+        {
+            Destroy(i.gameObject);
+        }
+        Circles.Clear();
+    }
+}
